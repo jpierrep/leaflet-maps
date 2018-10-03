2e89ff81f96dc8957ed69540360bc9e654263b37
Jean Pierre <jpena.suazo@gmail.com>
2018-10-03 18:20:42 -0300

    añade visualizacion plantas y puntos

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+'use strict'
+
 var express=require("express");
+
 var app=express();
 var path=require('path');
+var sql = require("mssql");
+var stringify = require('json-stringify');
+
+// config for your database
+var config = {
+    user: 'targit',
+    password: 'targit2015*',
+    server: '192.168.100.14', 
+    database: 'Inteligencias' 
+};
 
 app.set("view engine","jade");
 
@@ -8,10 +21,107 @@ app.get("/",function(req,res){
  
     var variable="variableeeee";
     var options=["a","b","c","d"];
-    res.render("index",{variable:variable,opciones:options});
+    var plantas=getPlantas().then(result=>{
+    //console.log(result);
+  var geoJSON= JSON.stringify(createGeoJSON(result));
+   //var geoJSON=createGeoJSON(result);
+
+
+    res.render("index",{variable:variable,opciones:options,geoJSON:geoJSON});
+
+    });
+   
+
 
 
 });
+
+
+function getPlantas(){
+    let query=`SELECT [nombre],[longitude],[latitude] ,ci.CENCO1_DESC as cenco1_desc
+    FROM [SISTEMA_CENTRAL].[dbo].[plantas] as p left join [SISTEMA_CENTRAL].[dbo].[centros_costos] as cc
+    on p.centro_costos_id=cc.id
+    left join Inteligencias.dbo.CENTROS_COSTO as ci
+    
+    on ci.CENCO2_CODI=cc.cencos_codigo collate SQL_Latin1_General_CP1_CI_AI  and ci.EMP_CODI=cc.empresa_id
+    where cc.deleted_at is null`;
+    
+    return new Promise(resolve=>{
+
+        entrega_resultDB(query).then(result=>{
+      
+          resolve(result);
+
+        });
+
+    });
+
+}
+
+function createGeoJSON(data){
+
+    var geojsonFeature = [{
+        "type": "Feature",
+        "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!",
+        "Group":"a"
+        },
+        "geometry": {
+        "type": "Point",
+        "coordinates": [-71.99404, 39.75621]
+        }
+        },
+        {
+        "type": "Feature",
+        "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!",
+        "Group":"a"
+        },
+        "geometry": {
+        "type": "Point",
+        "coordinates": [41.69861,-72.724141]
+        }
+        }];
+
+     let nuevoData=  data.map(element=>{
+        return {"type":"Feature",
+        "geometry": {
+        "type": "Point",
+        "coordinates": [element.longitude,element.latitude]
+        }
+        ,"properties": {"Group":"a","name": "Coors Field"}
+       };
+
+    });
+    return nuevoData;
+
+}
+
+function entrega_resultDB(queryDB){
+    return new Promise(resolve=>{ 
+    // const fruit = request.params.parame;
+  
+      // connect to your database
+     sql.connect(config).then((pool)=>{
+       return pool.request().query(queryDB)
+     }).then(result=>{
+      resolve(result);
+  
+      }).catch(err=>{
+       console.log(err);
+     })
+  
+  
+     }); 
+  
+  
+  
+  }
+
 
 app.listen(8000,()=>{
     console.log("Servidor corriendo en puerto 8000")
