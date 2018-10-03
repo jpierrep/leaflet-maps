ecd2ab30c9fb9d754feb7db5b166972e013d155a
Jean Pierre <jpena.suazo@gmail.com>
2018-10-03 16:52:54 -0300

    añadido integracion node leaflet

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express=require("express");
 var app=express();
+var path=require('path');
 
 app.set("view engine","jade");
+
 app.get("/",function(req,res){
- res.render("index");
+ 
+    var variable="variableeeee";
+    var options=["a","b","c","d"];
+    res.render("index",{variable:variable,opciones:options});
 
 
 });
 
-app.listen(8000);
+app.listen(8000,()=>{
+    console.log("Servidor corriendo en puerto 8000")
+});
