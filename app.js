4bdc28f83058c85a9d42f6392cb32cd116550de0
Jean Pierre <jpena.suazo@gmail.com>
2022-11-04 21:42:35 -0300

    migra mapa supervisores a ejs

--- app.js
+++ app.js
@@ -625,6 +625,80 @@ let unique = (value, index, self) => {
 });
 
 
+app.get("/getGuardias",function(req,res){
+ 
+  var variable="variableeeee";
+  var options=["a","b","c","d"];
+
+  
+
+
+  var plantas=getPlantas().then(result=>{
+  //console.log(result);
+var dataResult=JSON.stringify(result);
+var geoJSON= JSON.stringify(createGeoJSON(result));
+
+var  cenco1=  result.map(value=>{
+  return value.cenco1_desc;
+});
+
+var  supervisores=  result.map(value=>{
+  return value.administrativo_nombre;
+});
+
+var distinctCenco1=getUnique(cenco1);
+console.log(distinctCenco1);
+
+var distinctSupervisores=getUnique(supervisores);
+console.log(distinctSupervisores)
+
+ //var geoJSON=createGeoJSON(result);
+
+
+ let colorArray=['#696969','#808080','#a9a9a9','#c0c0c0','#dcdcdc','#2f4f4f','#556b2f','#8b4513','#6b8e23','#a0522d','#a52a2a','#2e8b57','#228b22','#191970','#006400','#708090','#8b0000','#808000','#483d8b','#b22222','#5f9ea0','#3cb371','#bc8f8f','#663399','#008080','#b8860b','#bdb76b','#cd853f','#4682b4','#d2691e','#9acd32','#20b2aa','#cd5c5c','#00008b','#4b0082','#32cd32','#daa520','#7f007f','#8fbc8f','#b03060','#d2b48c','#66cdaa','#9932cc','#ff0000','#ff4500','#00ced1','#ff8c00','#ffa500','#ffd700','#6a5acd','#ffff00','#c71585','#0000cd','#40e0d0','#7fff00','#00ff00','#9400d3','#ba55d3','#00fa9a','#8a2be2','#00ff7f','#4169e1','#e9967a','#dc143c','#00ffff','#00bfff','#f4a460','#9370db','#0000ff','#a020f0','#f08080','#adff2f','#ff6347','#da70d6','#d8bfd8','#b0c4de','#ff7f50','#ff00ff','#1e90ff','#db7093','#f0e68c','#fa8072','#eee8aa','#ffff54','#6495ed','#dda0dd','#add8e6','#87ceeb','#ff1493','#7b68ee','#ffa07a','#afeeee','#ee82ee','#98fb98','#87cefa','#7fffd4','#ffe4b5','#ffdab9','#ff69b4','#ffc0cb','#ff69b4','#ffc0cb']
+
+let unique = (value, index, self) => {
+  return self.indexOf(value) == index;
+}
+
+let distinctSupervisoresId = result.map(x =>  {return x['administrativo_id'] }).filter(unique)
+ console.log('supervisores',distinctSupervisoresId)
+ 
+ var supervisoresColor=distinctSupervisoresId.map((supervisor,index)=>{return {administrativo_id:supervisor,color:colorArray[index]}})
+ console.log("sup coloooorr",supervisoresColor)
+
+
+ getResumenSupervisor().then(resultSupervisor=>{
+
+ console.log("el result superv",resultSupervisor)
+   resultSupervisor.filter(x=>x.ADMINISTRATIVO_ID).map(supervisor=>{
+    let sup=supervisoresColor.find(x=>x.administrativo_id==supervisor.ADMINISTRATIVO_ID)
+    if (sup){supervisor.COLOR=sup.color} else supervisor.COLOR='#FF0000'
+  // supervisor.COLOR=supervisoresColor.find(x=>x.administrativo_id==supervisor.ADMINISTRATIVO_ID).color
+   //supervisor.COLOR='#FF0000'
+   //supervisor.DOT_VENDIDA= supervisor.DOT_VENDIDA.toFixed(2)
+      return  supervisor
+
+   })
+   console.log("el sup",resultSupervisor[0])
+
+  res.render("guardias.ejs",{variable:variable,opciones:options,dataResult:dataResult
+      ,geoJSON:geoJSON,distinctCenco1:distinctCenco1,distinctSupervisores:distinctSupervisores,supervisoresColor:JSON.stringify(supervisoresColor)
+     ,resumenSupervisor:supervisoresColor,resultSupervisor:resultSupervisor});
+
+ })
+
+
+
+
+  });
+ 
+
+
+
+});
+
+
  function getUnique(data){
     let unique = (value, index, self) => {
         return self.indexOf(value) == index;
